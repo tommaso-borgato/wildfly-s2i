6e52dd5b89a62475b937481b5a73f51c9e0d69e4
JF Denise <jdenise@redhat.com>
2021-04-30 12:20:54 +0200

    add ref

--- tools/github-deploy.sh
+++ tools/github-deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ -z "${WILDFLYS2I_FORK_NAME}" ];
+then
+  WILDFLYS2I_FORK_NAME="wildfly"
+fi
+
+currentBranch=$(git branch | sed -n '/\* /s///p')
+
 # this is the path to the repo the build should run from. It should not begin with a leading /.
-WILDFLY_REPO="wildfly/wildfly-s2i/deployments"
+WILDFLY_REPO="${WILDFLYS2I_FORK_NAME}/wildfly-s2i/deployments"
 
 if [ -z "${WILDFLYS2I_GITHUB_PAT}" ];
 then
@@ -11,11 +18,12 @@ then
     echo "Then export the token in your current shell: export WILDFLYS2I_GITHUB_PAT=... and re-run this script. Your deployment action will run as the production deploy action in github actions."
     echo
 else
+    echo "Creating deployment workflow for '${WILDFLY_REPO}' branch '${currentBranch}'"
     curl \
     -X POST \
     -H "Authorization: token $WILDFLYS2I_GITHUB_PAT" \
     -H "Accept: application/vnd.github.ant-man-preview+json" \
     -H "Content-Type: application/json" \
     https://api.github.com/repos/${WILDFLY_REPO} \
-    --data '{"ref": "master", "required_contexts": [], "environment": "production"}'
+    --data '{"ref": "'"${currentBranch}"'", "required_contexts": [], "environment": "production"}'
 fi
