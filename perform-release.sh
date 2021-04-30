ec78a7ea69fda24215a9a5788920097db9da5f7a
JF Denise <jdenise@redhat.com>
2021-04-30 16:27:12 +0200

    fix a mising push

--- perform-release.sh
+++ perform-release.sh
@@ -4,7 +4,7 @@ docker tag quay.io/jfdenise/wildfly-runtime-centos7:test-dep-1.0 quay.io/jfdenis
 docker tag quay.io/jfdenise/wildfly-runtime-centos7:test-dep-1.0 quay.io/jfdenise/wildfly-runtime-centos7:latest-foo
 docker push quay.io/jfdenise/wildfly-centos7:test-dep-1.0-foo
 docker push quay.io/jfdenise/wildfly-centos7:latest-foo
-docker push 
+docker push quay.io/jfdenise/wildfly-runtime-centos7:test-dep-1.0-foo
 docker push quay.io/jfdenise/wildfly-runtime-centos7:latest-foo
 docker tag quay.io/jfdenise/taskrs-app:latest quay.io/jfdenise/taskrs-app:latest-foo
 docker push quay.io/jfdenise/taskrs-app:latest-foo
