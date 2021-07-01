0bcf18ea5b318a41b63fa01d16b53ab6df0ad609
JF Denise <jdenise@redhat.com>
2021-07-01 10:49:03 +0200

    add trace

--- test/test-app-custom/extensions/install.sh
+++ test/test-app-custom/extensions/install.sh
@@ -7,5 +7,7 @@ fi
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
+echo COPYING MODULES ${injected_dir}/modules
+ls -l ${injected_dir}/modules
 install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
