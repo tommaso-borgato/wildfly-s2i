ea7883cc426a45b8fd8206b33df7c37743a6f3ea
JF Denise <jdenise@redhat.com>
2022-03-29 15:29:09 +0200

    maven artifact cleanup

--- wildfly-modules/jboss/container/wildfly/final-setup/configure.sh
+++ wildfly-modules/jboss/container/wildfly/final-setup/configure.sh
@@ -9,4 +9,9 @@ cp -r $JBOSS_HOME/standalone/deployments/* /deployments
 rm -rf $JBOSS_HOME/standalone/deployments
 ln -s /deployments $JBOSS_HOME/standalone/deployments
 
+# cleanup maven artifacts
+# A dependency of maven-install-plugin when building artifacts.
+# CVE CVE-2017-1000487
+rm -rf $GALLEON_LOCAL_MAVEN_REPO/org/codehaus/plexus
+
 chown -R jboss:root $HOME
