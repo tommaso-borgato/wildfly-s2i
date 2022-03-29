dc23484d7628277b3d1a223ff4eb7f8a2c6807b0
JF Denise <jdenise@redhat.com>
2022-03-29 15:29:09 +0200

    Delete codehaus/plexus artifacts

--- wildfly-modules/jboss/container/wildfly/final-setup/configure.sh
+++ wildfly-modules/jboss/container/wildfly/final-setup/configure.sh
@@ -9,4 +9,7 @@ cp -r $JBOSS_HOME/standalone/deployments/* /deployments
 rm -rf $JBOSS_HOME/standalone/deployments
 ln -s /deployments $JBOSS_HOME/standalone/deployments
 
+# cleanup maven artifacts
+rm -rf $GALLEON_LOCAL_MAVEN_REPO/org/codehaus/plexus/plexus
+
 chown -R jboss:root $HOME
